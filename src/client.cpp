3766d0a4bafac0009de279adf233768483e34c36
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-06-29 22:34:32 +0200

    testing client works - ends after send msg

--- src/client.cpp
+++ src/client.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 int main (int argc, char* argv[]) {
 
-	std::cout << "Client start" << std::endl;
 	std::cout << "supported cmds:\n"<< "subscribe:LivingRoom\npublish:LivingRoom:HelloMsg\n"<<std::endl;
+	std::cout << "Client start" << std::endl;
 
 	string host = "localhost";
 	int port = 1886;
@@ -20,14 +20,14 @@ int main (int argc, char* argv[]) {
 		ClientSocket client_socket ( host, port );
 		string reply;
 
-		while (1){
+
 			client_socket << argv[1];
 			client_socket >> reply;
 			std::cout << "Client got msg: " << reply <<std::endl;
-		}
+
 
 	} catch (...){
-		std::cout<<"!EXCEPTION: can not connect!"<<endl;
+		std::cout<<"connection lost."<<endl;
 	}
 
 	std::cout << "Client end" << std::endl;
